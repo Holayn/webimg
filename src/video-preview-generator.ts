1201590dff650410269f9aadc27cd18ed1cf5756
Holayn <kwwong1597@gmail.com>
2025-12-15 21:20:42 -0500

    Remove old video preview before generating new one.

--- src/video-preview-generator.ts
+++ src/video-preview-generator.ts
@@ -1,5 +1,7 @@
 import { execa } from 'execa';
 import { File, FileResizeSize } from "./file.js";
+import { fileExists } from './util.js';
+import { unlink } from 'node:fs/promises';
 
 export class VideoPreviewGeneratorError extends Error {
   constructor(message: string, cause?: unknown) {
@@ -13,6 +15,10 @@ export async function generateVideoPreview({ file, size }: { file: File, size: F
     const isHDR = file.metadata?.WebImg.HDR || false;
 
     try {
+      if (await fileExists(file.getVideoPreviewDest(size.name))) {
+        await unlink(file.getVideoPreviewDest(size.name));
+      }
+      
       const resizeVf = `scale=-1:${size.image?.height}`;
       await execa('ffmpeg', ['-i', file.path, '-vf', isHDR ? `zscale=t=linear:npl=100,format=gbrpf32le,tonemap=hable,zscale=t=bt709:m=bt709:r=tv,format=yuv420p,${resizeVf}` : resizeVf, '-vframes', '1', file.getVideoPreviewDest(size.name)]);
     } catch (e) {
